875b40221e8ef89df45878c09f5a557e72a98c42
WiseRod <wise3edar4@gmail.com>
2026-03-24 20:52:49 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="William Serrano",                       # Tu nombre
+    author_email="wise3edar4@gmail.com",            # Tu correo electrónico
+    url="https://github.com/WiseRodx/tercerRepo",   # URL del proyecto
+)
